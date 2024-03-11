2fecc9a413c3a5b128a7fa2161cf78b014211958
ISMAIL ALIYU BARDE <88933487+Aliyubarde97@users.noreply.github.com>
2024-03-11 11:02:50 +0100

    update the footer

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 
 import logo from '../../assets/img/eConiaSoft-Final2.png'
+import { FacebookLogo, LinkedinLogo, TwitterLogo } from "phosphor-react"
 const FooterComponent = () => {
   return (
      <>
      <div className="relative w-full text-white bg-bg-color">
       <div className="container">
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 p-20 gap-20">
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 p-20 gap-20 cursor-pointer ">
           <div className="flex flex-col">
             <Link to={""}>
               <img src={logo} alt=""  />
@@ -14,33 +15,43 @@ const FooterComponent = () => {
             </Link>
 
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-3">
             <p className="uppercase">Quick link</p>
             <ul>
-              <li><Link to={""}></Link></li>
-              <li><Link to={""}></Link></li>
-              <li><Link to={""}></Link></li>
-              <li><Link to={""}></Link></li>
+              <li className="my-5"><Link to={""}>Home</Link></li>
+              <li className="my-5"><Link to={""}>Features</Link></li>
+              <li className="my-5"><Link to={""}>Product</Link></li>
+              <li className="my-5"><Link to={""}>Pricing</Link></li>
+              <li className="my-5"><Link to={""}>Contact</Link></li>
             </ul>
 
           </div>
-          <div className="flex flex-col">
-            <p>Quick link</p>
+          <div className="flex flex-col gap-3">
+            <p className="uppercase">service</p>
             <ul>
-              <li><Link to={""}></Link></li>
-              <li><Link to={""}></Link></li>
-              <li><Link to={""}></Link></li>
-              <li><Link to={""}></Link></li>
+              <li className="my-5"><Link to={""}>Shared Web</Link></li>
+              <li className="my-5"><Link to={""}>Hosting</Link></li>
+              <li className="my-5"><Link to={""}>vps Hosting</Link></li>
+              <li className="my-5"><Link to={""}>Dedicated server</Link></li>
+              <li className="my-5"><Link to={""}>cloud Hosting</Link></li>
+              <li className="my-5"><Link to={""}>Domain Names</Link></li>
+              <li className="my-5"><Link to={""}>License</Link></li>
+
             </ul>
 
           </div>
-          <div className="flex flex-col">
-            <p>Quick link</p>
+          <div className="flex flex-col gap-3">
+            <p className="uppercase">Conatct</p>
             <ul>
-              <li><Link to={""}></Link></li>
-              <li><Link to={""}></Link></li>
-              <li><Link to={""}></Link></li>
-              <li><Link to={""}></Link></li>
+              <li className="my-5"><Link to={""}>Support@E_ConiaSoft.com</Link></li>
+              <li className="my-5"><Link to={""}>+234567890</Link></li>
+              <li className="my-5"><Link to={""}>Follow us</Link></li>
+             <div className="flex justify-start items-center gap-3">
+              <Link to={""}><FacebookLogo size={32} /></Link>
+              <Link to={""}><TwitterLogo size={32} /></Link>
+              <Link to={""}><LinkedinLogo size={32} /></Link>
+
+             </div>
             </ul>
 
           </div>
